07a33cc38d6d775f50936b1986164d93c531e427
robi-ciresa <157184937+robi-ciresa@users.noreply.github.com>
2024-02-01 16:01:11 +0100

    update1

---
+++ assets/js/counter.js
@@ -0,0 +1,48 @@
+var showNumber = document.querySelector(".showNumber");
+var plusBtn = document.querySelector(".plusBtn");
+var minusBtn = document.querySelector(".minusBtn");
+var batteryColor = document.querySelector(".batteryColor")
+var errorMsg = document.querySelector(".errorMsg");
+
+var number = 0;
+
+function batteryCharge(number) {
+    var backgroundColor = "";
+    if (number<30) {
+        backgroundColor = "red";
+    }
+    else if (number<70) {
+        backgroundColor = "yellow";
+    }
+    else {
+        backgroundColor = "green";
+    }
+    return backgroundColor;
+}
+
+plusBtn.addEventListener("click", function(){
+    if (number<100) 
+        { number++;
+        errorMsg.innerHTML = ""
+        showNumber.innerHTML = number;
+        batteryColor.style.width = number+"%";
+        batteryColor.style.backgroundColor = batteryCharge(number);}
+    else 
+        {showNumber.innerHTML = number+" error!";
+            errorMsg.innerHTML = "you can't select a nummer so high!"
+        }
+});
+
+minusBtn.addEventListener("click", function(){
+    if (number>0) 
+        {number--;
+        errorMsg.innerHTML = ""
+        showNumber.innerHTML = number;
+        batteryColor.style.width = number+"%";
+        batteryColor.style.backgroundColor = batteryCharge(number);}
+    else 
+        {showNumber.innerHTML = number+" error!";
+            errorMsg.innerHTML = "you can't select a nummer so low!"
+        }
+});
+
