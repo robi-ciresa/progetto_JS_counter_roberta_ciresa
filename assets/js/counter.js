dd3383344309726180836d180781740eb9b43fc9
robi-ciresa <157184937+robi-ciresa@users.noreply.github.com>
2024-02-10 13:47:27 +0100

    update5

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,8 +1,48 @@
-var showNumber = document.querySelector(".showNumber");
-var plusBtn = document.querySelector(".plusBtn");
-var minusBtn = document.querySelector(".minusBtn");
-var batteryColor = document.querySelector(".batteryColor")
-var errorMsg = document.querySelector(".errorMsg");
+let h1= document.createElement('h1');
+document.querySelector(".container").appendChild(h1);
+h1.innerHTML = "This project is about a counter.";
+
+let h2= document.createElement('h2');
+document.querySelector(".container").appendChild(h2);
+h2.innerHTML = 'You have to click "+" to increase your battery level and "-" to decrease that.<br/>Remind, you can select a number between 0 and 100.';
+
+let section1= document.createElement('section');
+section1.setAttribute("class","row");
+document.querySelector(".container").appendChild(section1);
+
+let divminusBtn = document.createElement('div');
+divminusBtn.setAttribute("class","col-3");
+section1.appendChild(divminusBtn);
+let minusBtn = document.createElement('button');
+minusBtn.textContent = "-";
+divminusBtn.appendChild(minusBtn);
+
+let showNumber = document.createElement('div');
+showNumber.setAttribute("class","col-6 showNumber");
+showNumber.innerHTML= "0";
+section1.appendChild(showNumber);
+
+let divplusBtn = document.createElement('div');
+divplusBtn.setAttribute("class","col-3");
+section1.appendChild(divplusBtn);
+let plusBtn = document.createElement('button');
+plusBtn.textContent = "+";
+divplusBtn.appendChild(plusBtn);
+
+let section2= document.createElement('section');
+document.querySelector(".container").appendChild(section2);
+
+let divbatteryCharge = document.createElement('div');
+divbatteryCharge.setAttribute("class","batteryCharge");
+section2.appendChild(divbatteryCharge);
+
+let batteryColor = document.createElement('div');
+batteryColor.setAttribute("class","batteryColor");
+divbatteryCharge.appendChild(batteryColor);
+
+let errorMsg = document.createElement('div');
+errorMsg.setAttribute("class","errorMsg");
+document.querySelector(".container").appendChild(errorMsg);
 
 var number = 0;
 
